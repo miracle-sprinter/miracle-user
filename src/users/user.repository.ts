c42452f88a1d2a277c955f6f7c1da707794063bd
BrianDYKim <18k7102dy@naver.com>
2023-04-12 23:29:50 +0900

    add: prisma 설정 완료

---
+++ src/users/user.repository.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async esrt(): Promise<string> {
+    return new Promise((resolve) => resolve('qwe'));
+  }
+}
